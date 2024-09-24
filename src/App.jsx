a6ecf5a26620d3642fc170481d4cb40fa43effac
sench-reviews <senchreviews@gmail.com>
2024-09-24 10:36:38 -0500

    final commit before first deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import Home from "./pages/Home";
 import SinglePlayer from "./pages/SinglePlayer";
+import NotFound from "./pages/NotFound";
+import CreatePlayer from "./pages/CreatePlayer";
 import Header from "./components/Header/Header";
+
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1>Puppy Bowl</h1>
       <Header />
-      <Home />
-      <SinglePlayer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:id" element={<SinglePlayer />} />
+        <Route path="/create" element={<CreatePlayer />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
